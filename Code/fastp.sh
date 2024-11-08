b64b34e39c420f139b4a0b35c83cd9d01ae5acb8
HarshaP22 <145778187+HarshaP22@users.noreply.github.com>
2024-11-09 00:01:17 +0530

    Add files via upload

---
+++ Code/fastp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#out=$2
+now=`date`
+
+while read inp
+do
+	suffix="_R1.fastq.gz"
+	bn=$(basename $inp $suffix)
+	path=$(dirname $inp)
+	fastq1=$path/$bn"_R1.fastq.gz"
+	fastq2=$path/$bn"_R2.fastq.gz"
+	
+	out1=$path/Trimmed/$bn"_trimmed_R1.fastq.gz"
+	out2=$path/Trimmed/$bn"_trimmed_R2.fastq.gz"
+	
+	echo $bn
+		(
+		echo "starts $now"
+
+		./fastp -i $fastq1 -I $fastq2 -o $out1 -O $out2
+
+		echo "ends $now"
+		) 1>>$out$bn".pipeline.log" 2>>$out$bn".pipeline.log" 
+done < $1
